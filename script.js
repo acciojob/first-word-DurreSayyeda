61b1c1ebe6392a8b2f7440667e6ed056890085aa
Sayyeda Afshan <syed1995.sa@gmail.com>
2024-01-16 21:17:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstWord(s) {
   // your code here
+	 if (s === '' || !s.includes(' ')) {
+    // If the input string is empty or does not contain any space, return the entire string
+    return s;
+  } else {
+    // Extract the first word by taking characters up to the first space
+    return s.substring(0, s.indexOf(' '));
+  }
 }
 
 // Do not change the code below
